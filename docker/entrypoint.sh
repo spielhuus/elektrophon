4cff0d933f6c5432f52e7934fee5c952c7536f9b
spielhuus <spielhuus@gmail.com>
2021-03-27 20:04:20 +0100

    update docker files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,12 +11,12 @@ OLD_GROUP=$(stat -c '%g' $JEKYLL_DIR/Makefile)
 function create_user()
 {
     groupadd -f -g $USER_GID $USERNAME 2> /dev/null # group
-    useradd -m -s /bin/bash -g $USERNAME $USERNAME  2> /dev/null # user
+    useradd -d $JEKYLL_DIR -M -s /bin/bash -g $USERNAME $USERNAME  2> /dev/null # user
     echo "$USERNAME ALL=NOPASSWD:ALL" >> /etc/sudoers
     chown -R $USERNAME:$USERNAME $JEKYLL_DIR
     chown -R $USERNAME:$USERNAME $GEM_HOME
-    chown -R $USERNAME:$USERNAME /home/jekyll
-    chown -R $USERNAME:$USERNAME /home/jekyll/.config
+#    chown -R $USERNAME:$USERNAME /home/jekyll
+#    chown -R $USERNAME:$USERNAME /home/jekyll/.config
 }
 
 # Create the user
@@ -25,5 +25,4 @@ create_user
 # Execute the rest of the script with the new user
 su -c "/entrypoint-user.sh \"$@\"" $USERNAME
 
-echo "reset uid:gid $OLD_USER:$OLD_GROUP"
 chown -R $OLD_USER:$OLD_GROUP $JEKYLL_DIR
