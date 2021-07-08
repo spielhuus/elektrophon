05fd36402648a0fc6c484818b8d6145074af1b12
spielhuus <37036732+spielhuus@users.noreply.github.com>
2021-07-08 11:42:02 +0000

    Deploying to gh-pages from  @ ddf4ab0207eff3da4d69caa8d73a109a0285b2a3 🚀

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,35 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
 });
+
+/* the image modal script */
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("item-slide");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
