62b6f9403268ff51006c2f92cf3241b4594edaae
spielhuus <spielhuus@gmail.com>
2021-11-15 18:43:01 +0100

    remove old files

--- modules/klang/firmware/Inc/constants.h
+++
@@ -1,8 +0,0 @@
-#ifndef __KLANG_CONSTANTS_H
-#define __KLANG_CONSTANTS_H
-
-#include "arm_math.h"
-
-#define NUM_VOICES 6
-
-#endif /* __KLANG_CONSTANTS_H */
--- modules/klang/firmware/Inc/wavetable.h
+++
@@ -1,9 +0,0 @@
-#ifndef __WAVETABLE_H
-#define __WAVETABLE_H
-
-#include "math.h"
-#include "arm_math.h"
-
-float32_t wt_sine[1024];
-
-#endif /* __WAVETABLE_H */
